3f546dab6a7ebd82b79ca61ccd0bcec26e7a8e8c
realYellowDev <41997217+realYellowDev@users.noreply.github.com>
2018-11-02 21:34:27 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,38 +2,4 @@
 
 # Einfaches Backup-Script
 
-SIKFILEPRE=${HOME}/sicherung
-
-SIKFILENAME=${SIKFILEPRE}_`date +%Y%m%d`.tar
-
-if [ $# -lt 1 ]; then
-
-    echo "Fehler: Kein Dateiname eingegeben"
-
-    echo "Benutzung: $0 datei.ext [datei2.ext] [datei.ext]"
-
-    exit 1
-
-else
-
-    echo ${SIKFILENAME} wird erzeugt
-
-    tar -cvf ${SIKFILENAME} "$@"
-
-    if [ $? -eq 0 ]
-
-    then
-
-        echo Dateien erfolgreich gesichert
-
-    else
-
-        echo Fehler bei Datensicherung
-
-        exit 2
-
-    fi
-
-    gzip $SIKFILENAME
-
-fi
+dialog --yesno "Bist du ein Linuxfan?" 15 60
