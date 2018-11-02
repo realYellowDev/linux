0c6e217eeba1d524545a942525f5b70d78691b01
realYellowDev <41997217+realYellowDev@users.noreply.github.com>
2018-11-02 21:25:49 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,39 @@
-echo "Im cool"
+#!/bin/bash
+
+# Einfaches Backup-Script
+
+SIKFILEPRE=${HOME}/sicherung
+
+SIKFILENAME=${SIKFILEPRE}_`date +%Y%m%d`.tar
+
+if [ $# -lt 1 ]; then
+
+    echo "Fehler: Kein Dateiname eingegeben"
+
+    echo "Benutzung: $0 datei.ext [datei2.ext] [datei.ext]"
+
+    exit 1
+
+else
+
+    echo ${SIKFILENAME} wird erzeugt
+
+    tar -cvf ${SIKFILENAME} "$@"
+
+    if [ $? -eq 0 ]
+
+    then
+
+        echo Dateien erfolgreich gesichert
+
+    else
+
+        echo Fehler bei Datensicherung
+
+        exit 2
+
+    fi
+
+    gzip $SIKFILENAME
+
+fi
